d8f1177e73b69445e53a26c6a1036a531d31e2a1
fangting <fangtingzhou@tamu.edu>
2019-12-07 11:09:36 -0600

    add coefficient functions

---
+++ R/dataGeneration.R
@@ -0,0 +1,5 @@
+.b1fun = function(x) exp(x)
+.b2fun = function(x) {2 * sin(pi / x)}
+.b3fun = function(x) {2 * sin(pi * x)}
+.b4fun = function(x) {2 * sin(2 * pi * x)}
+.b5fun = function(x) {2 * sin(8 * pi * x)}
